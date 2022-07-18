ca7a7a77d7deb919e666cff71b4b9dd97217f7f0
Matthew <matthewjm99@gmail.com>
2022-07-18 22:46:45 +1000

    Updated packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { io } from 'socket.io-client'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'untitled'
+  title = 'KIT'
+
+  constructor() {
+    const socket = io('http://10.0.0.50:3000')
+  }
 }
