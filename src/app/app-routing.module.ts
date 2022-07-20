4789ac8104ddefc6c3b875d7eab4185b12174f43
Matthew <matthewjm99@gmail.com>
2022-07-20 18:10:56 +1000

    Guard secure routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router'
+import { JwtGuard } from './guards/jwt-guard'
 
 const routes: Routes = [
   {
@@ -13,11 +14,13 @@ const routes: Routes = [
       import('./pages/auth/tour/tour.module').then((m) => m.TourPageModule),
   },
   {
+    canActivate: [JwtGuard],
     path: 'home',
     loadChildren: () =>
       import('./pages/main/home/home.module').then((m) => m.HomePageModule),
   },
   {
+    canActivate: [JwtGuard],
     path: 'home/message/:id',
     loadChildren: () =>
       import('./pages/main/view-message/view-message.module').then(
--- src/app/pages/main/home/home.page.ts
+++ src/app/pages/main/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core'
 import { io } from 'socket.io-client'
 import { DataService, Message } from '../../../services/data.service'
+import { removeTokens } from '../../../functions/local-storage'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-home',
@@ -8,8 +10,13 @@ import { DataService, Message } from '../../../services/data.service'
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private data: DataService) {
+  constructor(private data: DataService, private router: Router) {
     const socket = io('http://10.0.0.50:3000/')
+  }
+
+  logout() {
+    removeTokens()
+    return this.router.navigate(['/auth'])
   }
 
   refresh(ev) {
