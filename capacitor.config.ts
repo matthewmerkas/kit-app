31f2dde966e1bf05f01e4bed19acc9d15e977c41
Matthew Merkas <matthew.merkas@gmail.com>
2022-08-04 18:57:16 +1000

    Package name changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.merkasoft.kit',
   appName: 'KIT',
   webDir: 'www',
   bundledWebRuntime: false,
